2b53ffd1a49ba8b087e1fd8b2143157f60c3c325
Oh Yang Taek <oyt950418@gmail.com>
2020-10-06 22:53:28 +0900

    form update

--- search/forms.py
+++ search/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    product = forms.CharField(label = 'Product')
-    product_version = forms.CharField(label = 'Product_version')
+    product = forms.CharField(required = False, label = 'Product')
+    product_version = forms.CharField(required = Fasle, label = 'Product_version')
